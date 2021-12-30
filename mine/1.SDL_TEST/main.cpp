e892e5d3065a6cbaf3512044557027889281d047
wenrui-Aupera <wenrui.zhong@auperastor.com>
2021-12-30 14:26:15 +0800

    add mine dir

---
+++ mine/1.SDL_TEST/main.cpp
@@ -0,0 +1,18 @@
+/**@file  main.c
+* @brief
+* @details
+* @author
+* @date        2021-12-30
+* @version     V1.0
+* @copyright
+*********************************************************************************
+*/
+
+#include "main.h"
+#include "SDL2/SDL.h"
+
+int main(int argc, char** argv)
+{
+
+    return 0;
+}
---
+++ mine/1.SDL_TEST/main.h
@@ -0,0 +1,14 @@
+/**@file  main.h
+* @brief
+* @details
+* @author
+* @date        2021-12-30
+* @version     V1.0
+* @copyright
+*********************************************************************************
+*/
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+
+#endif
